33faeb18bceda4b9ce358a1200729035f17dddb4
JuanCrlz45 <JuanCadxd4@gmail.com>
2025-09-03 10:31:10 -0500

    hola p

--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ namespace Proyecto001
     {
         static void Main(string[] args)
         {
+            Console.Write("Hola pe Batedia, como te iamas?: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("tu edad p: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.Write("y mides?: ");
+            decimal estatura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine($"Mi nombre es {nombre}, mi edad es {edad} y mi estatura es {estatura:N2} ");
+
+
+
         }
     }
 }
